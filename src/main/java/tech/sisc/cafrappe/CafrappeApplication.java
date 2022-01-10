6b6810d9577a99f4acf3b81a8dfa75e78111a285
Joao Basanta <joao.basa.18@gmail.com>
2022-01-10 17:06:01 -0500

    Cambios

---
+++ src/main/java/tech/sisc/cafrappe/CafrappeApplication.java
@@ -0,0 +1,13 @@
+package tech.sisc.cafrappe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafrappeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafrappeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/sisc/cafrappe/CafrappeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sisc.cafrappe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafrappeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
